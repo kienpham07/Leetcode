dda00ad2f791bc9ea7cf4a7ea0c12e288b7b6c58
kienpham1210 <kienpham121007@gmail.com>
2025-10-24 22:28:50 -0400

    Question 1 (Easy): Two Sum - Sol 2 - Optimal

---
+++ Easy/1-twosum-2-optimal.py
@@ -0,0 +1,11 @@
+#Question 1 (Leetcode): Two-Sum - Solution 2 (Optimal)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # val: index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
