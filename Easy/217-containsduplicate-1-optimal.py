deb37db6bb7d6a09366bac9a64c58e3fb70f117d
kienpham1210 <kienpham121007@gmail.com>
2025-10-27 15:18:56 -0400

    Question 217 (Easy): Contains Duplicate - Sol 1 - Optimal

--- Easy/217-containsduplicate-1-optimal.py
+++ Easy/217-containsduplicate-1-optimal.py
@@ -1,5 +1,4 @@
-# Question 36 (217): Contains Duplicate - Solution 1 (Optimal)
-# Use hash set
+#Question 217 (Leetcode): Contains Duplicate - Solution 1 (Optimal)
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
@@ -9,4 +8,16 @@ class Solution:
             if num in hash_map:
                 return True
             hash_map.add(num)
+        return False
+
+
+#Another solution -Use hash_map
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash_map = {}
+
+        for num in nums:
+            if num in hash_map:
+                return True
+            hash_map[num] = 1
         return False
