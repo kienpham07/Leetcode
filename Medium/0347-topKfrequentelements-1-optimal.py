6e172a331ab5ecdf7c92b33781fb3d5f6aa6e8ec
kienpham1210 <kienpham121007@gmail.com>
2025-11-03 16:39:10 -0500

    File

---
+++ Medium/0347-topKfrequentelements-1-optimal.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash_map = {}
+        count = [[] for i in range(len(nums) + 1)]
+        for num in nums:
+            hash_map[num] = 1 + hash_map.get(num, 0)
+
+        for key, value in hash_map.items():
+            count[value].append(key)
+
+        res = []
+
+        for i in range(len(count) - 1, 0, -1):
+            for num in count[i]:
+                res.append(num)
+                if (len(res) == k):
+                    return res
