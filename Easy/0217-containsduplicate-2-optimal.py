90ba0758ecb645af2ae1c3061b0c0c4667f5fbcc
kienpham1210 <kienpham121007@gmail.com>
2025-11-21 14:43:00 -0500

    File

---
+++ Easy/0217-containsduplicate-2-optimal.py
@@ -0,0 +1,37 @@
+#Question 217 (Leetcode): Contains Duplicate - Solution 2 (Optimal)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+
+        freq = [0] * 26
+
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
+
+        for i in range(26):
+            if freq[i] != 0:
+                return False
+
+        return True
+
+
+#Solution 3: Using defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+
+        count = defaultdict(int)
+
+        for letter in s:
+            count[letter] += 1
+
+        for letter in t:
+            count[letter] -= 1
+            if count[letter] < 0:
+                return False
+
+        return True
